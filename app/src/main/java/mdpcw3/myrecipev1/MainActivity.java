89e6e4b4232be5068b8ef428df4cabb1a6542868
Ameer Sorne <ameersorne@gmail.com>
2017-11-25 06:04:41 +0800

    file description

--- app/src/main/java/mdpcw3/myrecipev1/MainActivity.java
+++ app/src/main/java/mdpcw3/myrecipev1/MainActivity.java
@@ -1,11 +1,19 @@
+/*
+*  MainActivity : Displays collection of available Recipe
+*                 Has Search function
+*                 Has Exit button
+*                 Can Add new recipe via NEW RECIPE button
+* */
+
 package mdpcw3.myrecipev1;
 
-import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Global Variables
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/mdpcw3/myrecipev1/RecipeDetail.java
+++ app/src/main/java/mdpcw3/myrecipev1/RecipeDetail.java
@@ -1,3 +1,11 @@
+/*
+*  RecipeDetail : Has 2 MODE:
+*                 Mode 1 - Empty textbox to enter new Recipe
+*                        - with Add, Clear, and Back button
+*                 Mode 2 - Loaded selected item from ListView in MainActivity
+*                        - with Edit, Save, Delete and Cancel button
+* */
+
 package mdpcw3.myrecipev1;
 
 import android.support.v7.app.AppCompatActivity;
