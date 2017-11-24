2454013456574f08449ff1469be7ac070a554e84
Ameer Sorne <ameersorne@gmail.com>
2017-11-25 06:39:26 +0800

    MainActivity and RecipeDetail 1. added global var 2. init() and setEvents() 3. Lifecycle Log.d 4. Add simple Intent under btnAdd (MainActivity( 5. TODOs

--- app/src/main/java/mdpcw3/myrecipev1/MainActivity.java
+++ app/src/main/java/mdpcw3/myrecipev1/MainActivity.java
@@ -7,18 +7,120 @@
 
 package mdpcw3.myrecipev1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //Global Variables
+    private EditText txtSearch;
+    private Button btnSearch, btnExit, btnAdd;
+    private ListView listView;
+    private boolean isNewRecipe, isViewRecipe;
+
+    //This method initialize settings and items
+    private void init(){
+        Log.d("MyRecipe","MainActivity: Initializing settings and items");
+        txtSearch = findViewById(R.id.txtSearch);
+
+        btnSearch = findViewById(R.id.btnSearch);
+        btnExit = findViewById(R.id.btnExit);
+        btnAdd = findViewById(R.id.btnAdd);
+
+        listView = findViewById(R.id.listView);
+
+        isNewRecipe = false;
+        isViewRecipe = false;
+    }
+
+    //This method initialize events
+    private void setEvents(){
+        Log.d("MyRecipe","MainActivity: Initializing events and listeners");
+        //Search method for Search Button
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+
+        //Exit method for Exit button
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+
+        //Add method for Add button
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),RecipeDetail.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //This method display the database in the listView
+    public void display(){
+        Log.d("MyRecipe","MainActivity: Retrieving data from database");
+        //TODO listView
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MyRecipe","=MainActivity onCreate()");
+
+        //Initialising variables and setting up events
+        init();
+        setEvents();
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("MyRecipe","=MainActivity onStart()");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("MyRecipe","=MainActivity onRestart()");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("MyRecipe","=MainActivity onResume()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("MyRecipe","=MainActivity onPause()");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("MyRecipe","=MainActivity onStop()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("MyRecipe","=MainActivity onDestroy()");
     }
 
 }
--- app/src/main/java/mdpcw3/myrecipev1/RecipeDetail.java
+++ app/src/main/java/mdpcw3/myrecipev1/RecipeDetail.java
@@ -10,12 +10,112 @@ package mdpcw3.myrecipev1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RecipeDetail extends AppCompatActivity {
+
+    //Global Variables
+    private EditText txtTitle, txtIns;
+    private Button btnBackCancel, btnClearDelete, btnEditSaveSubmit;
+    private boolean isNewRecipe, isViewRecipe;
+
+    //This method initialize settings and items
+    private void init(){
+        Log.d("MyRecipe","MainActivity: Initializing settings and items");
+        txtTitle = findViewById(R.id.txtTitle);
+        txtIns = findViewById(R.id.txtIns);
+
+        btnBackCancel = findViewById(R.id.btnBackCancel);
+        btnClearDelete = findViewById(R.id.btnClearDelete);
+        btnEditSaveSubmit = findViewById(R.id.btnEditSaveSubmit);
+
+        //TODO : get bundle/intent
+        //isNewRecipe = get;
+        //isViewRecipe = get;
+        //txtTitle.setText();
+        //txtIns.setText();
+
+        //button state
+
+    }
+
+    //This method initialize events
+    private void setEvents(){
+        Log.d("MyRecipe","RecipeDetail: Initializing events and listeners");
+
+        //onClick Listener for btnBackCancel
+        btnBackCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+
+        //onClick Listener for btnClearDelete
+        btnClearDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+
+        //onClick Listener for btnEditSaveSubmit
+        btnEditSaveSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
+        Log.d("MyRecipe","@RecipeDetail onCreate()");
+
+        //Initialising variables and setting up events
+        init();
+        setEvents();
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("MyRecipe","@RecipeDetail onStart()");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("MyRecipe","@RecipeDetail onRestart()");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("MyRecipe","@RecipeDetail onResume()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("MyRecipe","@RecipeDetail onPause()");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("MyRecipe","@RecipeDetail onStop()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("MyRecipe","@RecipeDetail onDestroy()");
     }
 }
