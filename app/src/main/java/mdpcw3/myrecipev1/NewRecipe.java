541e03da2a7d7de38b0dbe9cf0e23fd367289d79
Ameer Sorne <ameersorne@gmail.com>
2017-11-24 16:47:01 +0800

    Summary: activities, layout, focusable 1. added Activities: NewRecipe, RecipeDetail 2. Added rotated layout for all 3, postfix: _rotate 3. created layout for MainActivity    3a: changed xml:android:focusable="true"                    android:focusableInTouchMode="true"

---
+++ app/src/main/java/mdpcw3/myrecipev1/NewRecipe.java
@@ -0,0 +1,13 @@
+package mdpcw3.myrecipev1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_recipe);
+    }
+}
